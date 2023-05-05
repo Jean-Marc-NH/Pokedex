27a006b3e5252f9455b0ea7e593e2835debda4ba
Jean Marc Nadeau Huamani <101827583+Jean-Marc-NH@users.noreply.github.com>
2023-05-05 18:59:19 -0500

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS favoritos;
+use favoritos;
+
+CREATE TABLE pokemons2(
+    numero      int(25) auto_increment not null,
+    Id      int(25),
+    Nombre  varchar(100),
+    CONSTRAINT pk_usuarios PRIMARY KEY(names)
+)
